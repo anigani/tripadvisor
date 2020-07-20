5366228959cdf5bacd6d6f32f33950bcf3d91441
Davit Badalyan <lessons2222@gmail.com>
2020-07-20 11:44:03 +0400

    WebDriverManager is added

--- src/test/java/com/tripadvisor/pages/BaseTest.java
+++ src/test/java/com/tripadvisor/pages/BaseTest.java
@@ -1,31 +1,31 @@
 package com.tripadvisor.pages;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
-    protected WebDriver DRIVER;
+    protected WebDriver driver;
 
     @BeforeClass // Runs this method before the first test method in the current class is invoked
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "F:\\Ani\\workspace\\tripadvisor\\src\\test\\resources\\chromedriver.exe");
-//        System.setProperty("webdriver.gecko.driver", "F:\\Ani\\workspace\\tripadvisor\\src\\test\\resources\\geckodriver.exe");
+        // It downloads and uses the last Chrome Driver
+        WebDriverManager.chromedriver().setup();
 
         // Create a new instance of the Chrome driver
-        DRIVER = new ChromeDriver();
+        driver = new ChromeDriver();
 //        DRIVER = new FirefoxDriver();
 
         // Put an Implicit wait, this means that any search for elements on the page could take the time the implicit wait is set for before throwing exception
-        DRIVER.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @AfterClass
     public void tearDown() {
-        DRIVER.quit();
+        driver.quit();
     }
 }
